480ed586ac709413e2d74003ba627eea7de182f2
Aranya Ray <aranya.ray1998@gmail.com>
2024-01-05 17:49:05 +0530

    Logger , Utils and Exceptions files

---
+++ src/utils.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import dill
+
+import numpy as np
+import pandas as pd
+
+from src.exceptions import CustomException
+
+def save_object(file_path,obj): # Used to save an object, used by us to save pickle files
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj, file_obj)# Dill used to create the pickle files
+
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):# Function used to load the pickle files
+
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
